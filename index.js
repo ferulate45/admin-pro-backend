41b2190910f3db3baffb4e3728a1c12d1677712a
Maria Fernanda Ulate Fonseca <fernanda-45@hotmail.es>
2022-06-22 14:14:51 -0600

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -32,6 +33,10 @@ app.use('/api/hospitales', require('./routes/hospitales'));
 app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/search', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 
 
